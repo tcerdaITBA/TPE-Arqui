97c2f8fae8aa5199585bd7e94c2923d06fd3327b
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2016-06-12 11:22:32 -0300

    Keyboard con Shift y CapsLock

--- RowDaBoat-x64barebones-0c82a8afedb3/Kernel/keyboardDriver.c
+++ RowDaBoat-x64barebones-0c82a8afedb3/Kernel/keyboardDriver.c
@@ -1,70 +1,45 @@
 #include "keyboardDriver.h"
+#include "keyboardmap.h"
 
 #define BUFFSIZE 128
+#define TRUE 1
+#define FALSE 0
+
 
 char get_key();
 int key_pressed();
-
-// Fuente: www.osdever.net/bkerndev/Docs/keyboard.htm
-/* KBDUS means US Keyboard Layout. This is a scancode table
-*  used to layout a standard US keyboard. I have left some
-*  comments in to give you an idea of what key is what, even
-*  though I set it's array index to 0. You can change that to
-*  whatever you want using a macro, if you wish! */
-unsigned char kbdus[128] =
-{
-    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
-  '9', '0', '-', '=', '\b',	/* Backspace */
-  '\t',			/* Tab */
-  'q', 'w', 'e', 'r',	/* 19 */
-  't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
-    0,			/* 29   - Control */
-  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
- '\'', '`',   0,		/* Left shift */
- '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
-  'm', ',', '.', '/',   0,				/* Right shift */
-  '*',
-    0,	/* Alt */
-  ' ',	/* Space bar */
-    0,	/* Caps lock */
-    0,	/* 59 - F1 key ... > */
-    0,   0,   0,   0,   0,   0,   0,   0,
-    0,	/* < ... F10 */
-    0,	/* 69 - Num lock*/
-    0,	/* Scroll Lock */
-    0,	/* Home key */
-    0,	/* Up Arrow */
-    0,	/* Page Up */
-  '-',
-    0,	/* Left Arrow */
-    0,
-    0,	/* Right Arrow */
-  '+',
-    0,	/* 79 - End key*/
-    0,	/* Down Arrow */
-    0,	/* Page Down */
-    0,	/* Insert Key */
-    0,	/* Delete Key */
-    0,   0,   0,
-    0,	/* F11 Key */
-    0,	/* F12 Key */
-    0,	/* All other keys are undefined */
-};		
+int isShiftKey(char k);
+int processKeyScanCode(char k);
+int chooseKeyboard(char k);
+int isAlpha(char k);
 
 static int len = sizeof(kbdus);
 static char buffer[BUFFSIZE];
 static unsigned int ret_index = 0;
 static unsigned int store_index = 0;
 static int buff_size = 0;
+static int shiftPressed = FALSE;
+static int capsLockPressed = FALSE;
+static int capsLockPressedCount = 0;
+static int capsLockActivated = FALSE;
+//static int altgrPressed = FALSE;
+
 
 void store_scancode() {
   if (buff_size == BUFFSIZE)
     return;
+
   char k = get_key();
-  if (k > 0 && k < 128) {
-    buff_size++;
-    buffer[store_index++] = k;  // guarda el scancode de la tecla
-  }
+
+  if (k > 0 && k < TOPCHARCODE) { /*Se apreto una tecla */
+    
+    if(!processKeyScanCode(k)) { //caso de un caracter a imprimir en pantalla
+      buff_size++;
+      buffer[store_index++] = k;  // guarda el scancode de la tecla 
+    }
+  } else if (k < 0) { /*Se solto una tecla */
+        processKeyScanCode(k + TOPCHARCODE);   
+    }
   if (store_index == BUFFSIZE)
     store_index = 0;
 }
@@ -76,5 +51,56 @@ int get_char() {
   buff_size--;
   if (ret_index == BUFFSIZE)
     ret_index = 0;
+  return chooseKeyboard(k);
+}
+
+int isShiftKey(char k) {
+ return ((k == LSHIFTCODE) || (k == RSHIFTCODE));
+}
+
+/* Retorna TRUE si se toco algun caracter especial como SHIFT, CAPSLOCK */
+int processKeyScanCode(char k){
+  switch(k){
+    case RSHIFTCODE:
+          shiftPressed = !shiftPressed;
+          return TRUE;
+          break;
+    case LSHIFTCODE:
+          shiftPressed = !shiftPressed;
+          return TRUE;
+          break;
+    case CAPSLOCK:
+          capsLockPressed = !capsLockPressed;
+          capsLockPressedCount += 1;
+          if(capsLockPressedCount % 2 == 0) { //fue apretado y soltado 
+            capsLockActivated = !capsLockActivated;
+            capsLockPressedCount = 0;
+          }
+          return TRUE;
+          break;
+   // case ALTGR:
+     //     altgrPressed = !altgrPressed;
+      //    return TRUE;
+        //  break;
+    default: 
+          return FALSE;
+          break;
+  }
+  return FALSE;
+
+}
+
+int chooseKeyboard(char k) {
+  if(shiftPressed)
+    return shiftkbdus[k];
+  if((capsLockPressed|| capsLockActivated) && isAlpha(k)) {
+    return shiftkbdus[k];
+  }
   return kbdus[k];
+}
+
+int isAlpha(char k) {
+  if(( k> QPOSITION && k< PPOSITION) || (k > APOSITION && k < LPOSITION) || (k > ZPOSITION && k< LPOSITION)) 
+    return TRUE;
+  return FALSE;
 }
