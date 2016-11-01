8a559c3fc7512c66fb6b214a0ef97493a41acd3a
Luciano Bianchi <luchob>
2016-11-01 19:15:57 -0300

    .h mutex

--- nanOS/Kernel/kernelMutex.c
+++ nanOS/Kernel/kernelMutex.c
@@ -1,3 +1,9 @@
+#include "processManager.h"
+#include "kernelMutex.h"
+#include "strings.h"
+
+#define NULL ((void *) 0)
+
 typedef struct {
   process * p;
   mutex_node * next;
@@ -14,19 +20,18 @@ typedef struct {
   mutex_queue process_queue;
 } mutex;
 
-#define LOCKED = 0;
-#define UNLOCKED = 1;
-
-#define BLOCKED = 0;
-#define READY = 1;
-
-#define MAX_MUTEXES = 64;
-
-#define NOT_OPEN = -1;
-
 static mutex open_mutexes[MAX_MUTEXES];
 
-int is_open(int key) {
+extern int test_lock(int * locknum);
+
+static process dequeue_process(mutex m);
+static void queue_process(mutex m, process * p);
+static void unblock_process(process * p);
+static void block_process(process * p);
+static void unblock_process(process * p);
+static int is_open(int key);
+
+static int is_open(int key) {
   return key < MAX_MUTEXES && open_mutexes[key] != NULL;
 }
 
@@ -56,7 +61,7 @@ int mutex_close(int key) {
 int mutex_lock(int key) {
   if (is_open(key)) {
     mutex m = open_mutexes[key];
-    if (test_lock(m.locked)) { // Returns 1 if locked, 0 if unlocked
+    if (test_lock(&m.locked)) { // Returns 1 if locked, 0 if unlocked
       process * p = get_current_process();
       block_process(p);
       queue_process(m, p);
@@ -78,15 +83,15 @@ int mutex_unlock(int key) {
   return NOT_OPEN;
 }
 
-void block_process(process * p) {
+static void block_process(process * p) {
   p->st = BLOCKED;
 }
 
-void unblock_process(process * p) {
+static void unblock_process(process * p) {
   p->st = READY;
 }
 
-void queue_process(mutex m, process * p) {
+static void queue_process(mutex m, process * p) {
   mutex_node = malloc();
   mutex_node.p = p;
   mutex_node->next = NULL;
@@ -98,7 +103,7 @@ void queue_process(mutex m, process * p) {
   m.process_queue->last = mutex_node;
 }
 
-process dequeue_process(mutex m) {
+static process dequeue_process(mutex m) {
   if (m.process_queue.first == NULL)
     return NULL;
 
